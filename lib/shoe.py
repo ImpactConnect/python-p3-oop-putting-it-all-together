a06e2773c0990d723c66e47c913c3f154ac9ce81
Omotayo <kehindeomotayo11@gmail.com>
2023-10-06 11:48:30 +0100

    Done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"
+        
+    def check_size(self):
+        if not isinstance(self.size, int):
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+        
+stan_smith = Shoe("Adidas", 9)
+stan_smith.size = "not an integer"
+stan_smith.check_size()
